303cfe7f9abf91731a519060f3c5efbb22738d42
DavidMorake1 <32597970+DavidMorake1@users.noreply.github.com>
2019-03-23 00:49:26 +0200

    Update 31.js

--- build/31.js
+++ build/31.js
@@ -139,7 +139,7 @@ var CoreLoginReconnectPage = /** @class */ (function () {
             };
             _this.username = site.infos.username;
           //  _this.siteUrl = site.infos.siteurl;
-            _this.siteName = site.infos.sitename;
+          //  _this.siteName = site.infos.sitename;
             // Check logoURL if user avatar is not set.
             if (_this.site.avatar.startsWith(site.infos.siteurl + '/theme/image.php')) {
                 _this.site.avatar = false;
